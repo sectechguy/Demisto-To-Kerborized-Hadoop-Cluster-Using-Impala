218038d3b09d33cdb69e13a3fdaa0c8b9970c05a
sectechguy <37934572+sectechguy@users.noreply.github.com>
2019-12-12 14:35:51 -0500

    Update Test_Impala_Connection.py

--- Test_Impala_Connection.py
+++ Test_Impala_Connection.py
@@ -3,6 +3,7 @@ import ssl
 from impala.dbapi import connect
 from impala.util import as_pandas
 import pandas as pd
+
 os.system("kinit -kt /etc/krb5.keytab user@DOMAIN")
 IMPALA_HOST = os.getenv('IMPALA_HOST', '<data_lake_server')
 conn = connect(host=IMPALA_HOST, port=21050, auth_mechanism='GSSAPI', use_ssl=True)
@@ -10,5 +11,6 @@ cursor = conn.cursor()
 cursor.execute('SHOW TABLES')
 tables = as_pandas(cursor)
 cursor.execute('select namestr, destinationhostname from proxy_table limit 10')  //test connection
+
 df = as_pandas(cursor)
 print (df)
